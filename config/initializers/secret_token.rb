a1b36c0bd42cced7d497082a19e9276c92110aa1
polosilva <polo.silva.a@gmail.com>
2012-02-11 16:42:10 -0300

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fake2::Application.config.secret_token = '8d78b99ebec9e5c792d1a86ba049f4067458c138b8775e9a304c7fe944a2526bd210c62cbb9a2646fc3b3acb6f9ca5dc829c1ed2de732aeb16aafd08c3dcde0b'
